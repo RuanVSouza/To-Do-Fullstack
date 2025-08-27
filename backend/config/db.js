167075ce17495c43438ee5e9ad3d24db38880bbc
RuanVSouza <ruan2014souza17@gmail.com>
2025-08-27 16:00:27 -0300

    chore: retirada opcoes depreciadas do mongoose

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const connectDBMongo = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/ToDoList', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
     console.log(`MongoDB conectado: ${conn.connection.host}`);
   } catch (err) {
